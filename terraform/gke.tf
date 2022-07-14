7d542eb87c8e12287b8844f6b00e4cc76b91bb47
Enzo Cesco <enzo.cesco@gmail.com>
2022-07-14 00:16:45 -0300

    update gke - vcp

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -8,20 +8,30 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
+  networking_mode = "VPC_NATIVE"
+  node_locations = ["southamerica-east1-a"]
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "192.168.0.0/18"
+    services_ipv4_cidr_block = "192.168.64.0/18"
+  }
+
 }
 
-# Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
+# Custom Node Pool
+resource "google_container_node_pool" "primary" {
+
   name       = "gke-node-pool"
   location   = "southamerica-east1"
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 2
 
   node_config {
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
@@ -31,7 +41,7 @@ resource "google_container_node_pool" "primary_nodes" {
       env = "develop"
     }
 
-    # preemptible  = true
+    preemptible  = false
     machine_type = "n1-standard-1"
     tags         = ["gke-node", "proyectodevopsort-gke"]
     metadata = {
